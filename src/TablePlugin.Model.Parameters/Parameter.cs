9baa65d7149bbb0806670a9f6ed938841cf24dca
zwer.27@yandex.ru <zwer.27@yandex.ru>
2021-12-06 15:39:10 +0700

    Сделан класс Parameter.

--- src/TablePlugin.Model.Parameters/Parameter.cs
+++ src/TablePlugin.Model.Parameters/Parameter.cs
@@ -6,7 +6,93 @@ using System.Threading.Tasks;
 
 namespace TablePlugin.Model.Parameters
 {
+    /// <summary>
+    /// Класс  дополнительных параметров журнального столика
+    /// </summary>
     public class Parameter
     {
+        /// <summary>
+        /// Имя параметра
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Минимальное значение параметра
+        /// </summary>
+        private double _minimum;
+
+        /// <summary>
+        /// Максимальное значение параметра
+        /// </summary>
+        private double _maximum;
+
+        /// <summary>
+        /// Значение параметра
+        /// </summary>
+        private double _value;
+
+        /// <summary>
+        /// Конструктор класса Параметр
+        /// </summary>
+        /// <param name="name">Имя параметра</param>
+        /// <param name="minimum">Минимальное значение</param>
+        /// <param name="maximum">Максимальное значение</param>
+        /// <param name="value">Значение</param>
+        public Parameter(string name, double minimum, double maximum, double value)
+        {
+            Name = name;
+            Minimum = minimum;
+            Maximum = maximum;
+            Value = value;
+        }
+
+        /// <summary>
+        /// Название параметра
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Минимальное значение параметра
+        /// </summary>
+        public double Minimum
+        {
+            get => _minimum;
+            set
+            {
+                _minimum = value;
+            }
+        }
+
+        /// <summary>
+        ///  Максимальное значение параметра
+        /// </summary>
+        public double Maximum
+        {
+            get => _maximum;
+            set
+            {
+                _maximum = value;
+            }
+        }
+
+        /// <summary>
+        /// Значение параметра
+        /// </summary>
+        public double Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+            }
+        }
+
     }
 }
